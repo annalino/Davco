867a866578e921dfef0ab87a19d4cdcdc7e04bda
Chris Herring <chris@chrisherring.co>
2018-03-31 16:47:28 +1100

    Added modal js to the index page Made a function to setup the modal and fire it on window load

--- assets/application.js
+++ assets/application.js
@@ -3,10 +3,10 @@ var last_known_scroll_position = 0;
 var ticking = false;
 function updateScollClass(scroll_pos) {
   element = document.querySelector(".master-header");
-  
+
   if(element){
     element.classList.toggle('scrolled', scroll_pos > 10);
-  }  
+  }
 }
 
 window.addEventListener('scroll', function(e) {
@@ -19,17 +19,17 @@ window.addEventListener('scroll', function(e) {
       updateScollClass(last_known_scroll_position);
       ticking = false;
     });
-     
+
     ticking = true;
 
   }
-  
+
 });
 
 
 
 // THIS IS THE JAVASCRIPT FOR THE DROPDOWN
-/* When the user clicks on the button, 
+/* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
@@ -51,8 +51,13 @@ window.onclick = function(event) {
   }
 }
 
+function openModal() {
+  var myModal = new Modal({
+    content: 'Happy birthday Dave! We thought you might like a website! Hopefully you can become the mancave man. The builder thats is sought after to make crazy mancaves all over the world. Then I want a job'
+  });
+
+  myModal.open();
+}
+
 // attempt at splash screen
-
-
-
-
+window.onload=function() { openModal() }
